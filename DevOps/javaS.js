24f79b20845af77877186ae45eaac66659a128dc
abhijeet-ray <abhijeetr776@gmail.com>
2023-09-24 11:06:57 +0530

    html pages

---
+++ DevOps/javaS.js
@@ -0,0 +1,32 @@
+// JavaScript for all pages
+const currentPage = window.location.pathname.split('/').pop();
+const navLinks = document.querySelectorAll('.nav-links a');
+
+navLinks.forEach(link => {
+    if (link.getAttribute('href') === currentPage) {
+        link.classList.add('active');
+    }
+});
+
+// JavaScript for chapter collapsible content
+const chapterHeaders = document.querySelectorAll('.course-chapter h2');
+
+chapterHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+        content.classList.toggle('expanded');
+    });
+});
+
+// JavaScript for smooth scrolling
+const smoothScrollLinks = document.querySelectorAll('a[href^="#"]');
+
+smoothScrollLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
